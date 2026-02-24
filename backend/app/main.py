40835e4b0867d47454d1f27febcc30d4cc6473ce
alancw2 <alancw2@illinois.edu>
2026-02-24 12:30:55 -0600

    fixed docker for web as well

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException 
+from fastapi.middleware.cors import CORSMiddleware
 import app.routers.clients as clients
 from app import schemas
 from app import storage
@@ -15,6 +16,15 @@ def get_client_by_id(client_id: int):
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(clients_router)
 app.include_router(sessions_router)
 
